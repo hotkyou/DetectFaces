877d48fb4e0b1fa77156bf80034324af467ca2a5
hotkyou <hottarakasi.stu@gmail.com>
2023-12-15 09:31:46 +0900

    delete enumLine & functionLine

--- detectmouth.py
+++ detectmouth.py
@@ -1,27 +1,52 @@
-from .resources.JP.enum import JP_kana, JP_line, JP_kana_list
+import pickle
+import lightgbm
+import numpy as np
+from .resources.JP.enum import *
 from .facemesh import FaceMesh
 
 class DetectMouth:
-    def __init__(self, image, assign):
+    def __init__(self, image):
+        self.filename = 'DetectFaces/trustycat.pkl'
         self.image = image
         self.landmarks_dict = FaceMesh(self.image).execute
-        self.assign = assign
         self.leftmouth = self.landmarks_dict[61]
         self.rightmouth = self.landmarks_dict[291]
         self.uppermouth = self.landmarks_dict[13]
         self.lowermouth = self.landmarks_dict[14]
-        print(self.leftmouth, self.rightmouth, self.uppermouth, self.lowermouth)
+        self.landmarks = [0,11,12,13,14,15,16,17,37,38,39,40,41,42,61,62,72,73,74,76,77,78,80,81,82,84,
+                85,86,87,88,89,90,91,95,96,146,178,179,180,181,183,184,185,191,267,268,269,270,
+                271,272,291,292,302,303,304,306,307,308,310,311,312,314,315,316,317,318,319,320,
+                321,324,325,375,402,403,404,405,407,408,409,415]
+        self.mouth = []
+        for i in self.landmarks:
+            self.mouth.extend([self.landmarks_dict[i]["x"], self.landmarks_dict[i]["y"], self.landmarks_dict[i]["z"]])
+        self.mouth = np.reshape(self.mouth, (1, -1))
     
     @property
     def execute(self):
-        result = JP_kana[self.assign]
-        if result.value is None:
-            mouthHorizontal = self.leftmouth["x"] - self.rightmouth["x"]
-            mouthVertical = self.uppermouth["y"] - self.lowermouth["y"]
-            HVdifference = abs(mouthHorizontal - mouthVertical)
-            print(HVdifference)
-            #if HVdifference <= 0.01:
-                
-            #if abs(mouthHorizontal - mouthVertical) 
+        
+        with open(self.filename, 'rb') as model_file:
+            model = pickle.load(model_file)
+            
+        result = model.predict_proba(self.mouth)
+        mouthresult = model.predict(self.mouth)
+        resulta = np.array(result[0])
+        result = resulta * 100
+
+        for i, percentage in enumerate(result):
+            print("Class {}: {:.2f}%".format(i, percentage))
+        
+        if mouthresult == 0:
+            result = JP_kana["A"]
+        elif mouthresult == 1:
+            result = JP_kana["I"]
+        elif mouthresult == 2:
+            result = JP_kana["U"]
+        elif mouthresult == 3:
+            result = JP_kana["E"]
+        elif mouthresult == 4:
+            result = JP_kana["O"]
+        else: 
+            result = None
             
         return str(result.value)
--- resources/JP/functions.py
+++ resources/JP/functions.py
@@ -1,138 +1,15 @@
 class Functions:
     def jp_a():
-        return None
+        return "A"
     
     def jp_i():
-        pass
+        return "I"
     
     def jp_u():
-        pass
+        return "U"
     
     def jp_e():
-        pass
+        return "E"
     
     def jp_o():
-        pass
-    
-    def jp_ka():
-        pass
-    
-    def jp_ki():
-        pass
-    
-    def jp_ku():
-        pass
-    
-    def jp_ke():
-        pass
-
-    def jp_ko():
-        pass
-
-    def jp_sa():
-        pass
-
-    def jp_shi():
-        pass
-
-    def jp_su():
-        pass
-
-    def jp_se():
-        pass
-
-    def jp_so():
-        pass
-
-    def jp_ta():
-        pass
-
-    def jp_chi():
-        pass
-
-    def jp_tsu():
-        pass
-
-    def jp_te():
-        pass
-
-    def jp_to():
-        pass
-
-    def jp_na():
-        pass
-
-    def jp_ni():
-        pass
-
-    def jp_nu():
-        pass
-
-    def jp_ne():
-        pass
-
-    def jp_no():
-        pass
-
-    def jp_ha():
-        pass
-
-    def jp_hi():
-        pass
-
-    def jp_fu():
-        pass
-
-    def jp_he():
-        pass
-
-    def jp_ho():
-        pass
-
-    def jp_ma():
-        pass
-
-    def jp_mi():
-        pass
-
-    def jp_mu():
-        pass
-
-    def jp_me():
-        pass
-
-    def jp_mo():
-        pass
-
-    def jp_ya():
-        pass
-
-    def jp_yu():
-        pass
-
-    def jp_yo():
-        pass
-
-    def jp_ra():
-        pass
-
-    def jp_ri():
-        pass
-
-    def jp_ru():
-        pass
-
-    def jp_re():
-        pass
-
-    def jp_ro():
-        pass
-
-    def jp_wa():
-        pass
-
-    def jp_wo():
-        pass
-
-    def jp_n():
-        pass
+        return "O"
